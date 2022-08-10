2b06b5ed643498329cab3848f95f91b808990cf4
allizwarttech <91635445+allizwarttech@users.noreply.github.com>
2022-08-10 11:05:58 +0100

    authentication and user account

--- src/app/account/views/personal-information/personal-details/personal-details.component.ts
+++ src/app/account/views/personal-information/personal-details/personal-details.component.ts
@@ -1,17 +1,24 @@
 import { select } from '@angular-redux/store';
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-personal-details',
   templateUrl: './personal-details.component.html',
   styleUrls: ['./personal-details.component.scss'],
 })
-export class PersonalDetailsComponent implements OnInit {
+export class PersonalDetailsComponent implements OnInit, AfterContentInit {
   @select((s) => s.userDetails.userDetails) userDetails$: any;
+  @select((s) => s.userDetails.isLoading) isLoading$: any;
   userData: any;
   constructor() {}
 
   ngOnInit(): void {
+    this.userDetails$.subscribe((e: any) => {
+      this.userData = e;
+    });
+  }
+
+  ngAfterContentInit(): void {
     this.userDetails$.subscribe((e: any) => {
       this.userData = e;
     });
