bdeab3dd15478887ae36c43faaf65bb4dc27895f
allizwarttech <91635445+allizwarttech@users.noreply.github.com>
2022-09-07 10:50:18 +0100

    Activity module and lettering stage one activity practice

--- src/app/practice/lrp-activity/services/lrp-activity.service.ts
+++ src/app/practice/lrp-activity/services/lrp-activity.service.ts
@@ -67,6 +67,102 @@ export class LrpActivityService {
       isChecked: false,
       type: AlphabetType.CONSONANT,
     },
+    {
+      id: 11,
+      name: 'k',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 12,
+      name: 'l',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 13,
+      name: 'm',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 14,
+      name: 'n',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 15,
+      name: 'o',
+      isChecked: false,
+      type: AlphabetType.VOWEL,
+    },
+    {
+      id: 16,
+      name: 'p',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 17,
+      name: 'q',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 18,
+      name: 'r',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 19,
+      name: 's',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 20,
+      name: 't',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 21,
+      name: 'u',
+      isChecked: false,
+      type: AlphabetType.VOWEL,
+    },
+    {
+      id: 22,
+      name: 'v',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 23,
+      name: 'w',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 24,
+      name: 'x',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 25,
+      name: 'y',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
+    {
+      id: 26,
+      name: 'z',
+      isChecked: false,
+      type: AlphabetType.CONSONANT,
+    },
   ];
 
   GetAlphabet() {
