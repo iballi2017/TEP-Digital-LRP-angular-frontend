c374d3d3d03b60f5e61ab5d548f16120e7dc3dcc
allizwarttech <91635445+allizwarttech@users.noreply.github.com>
2022-11-16 12:43:36 +0100

    update

---
+++ src/app/practice/practice-menu/practice-menu.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PracticeMenuComponent } from './practice-menu.component';
+
+describe('PracticeMenuComponent', () => {
+  let component: PracticeMenuComponent;
+  let fixture: ComponentFixture<PracticeMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PracticeMenuComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PracticeMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
